437870426529d96610bc992e46b98c09d48b6c81
TashtandyevBaysal <nurlanibakov1@gmail.com>
2023-12-22 18:42:09 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import FirstComponent from './components/FirstComponent';
 import Props1 from './components/Props1';
 import Meter from './useState_useEffect';
 import ItemList from './useCallback--useMemo';
+import {Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+import Home from './Components2/Home.js';
+import About from './Components2/About.js';
+import Contact from './Components2/Contact.js';
+import Error from './Components2/Error.js';
 
 
 function App() {
@@ -12,12 +17,32 @@ function App() {
       {/* <FirstComponent/>
       <Props1 colorItem ='red'/>
       <Props1 /> */}
-      
+
       {/* <Events/> */}
 
       {/* <Meter>{ Meter }</Meter> */}
-      
-      <ItemList items={items} />
+
+      {/* <ItemList items={items} /> */}
+      {/* <BrowserRouter>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+    </BrowserRouter> */}
     </div>
   );
 }
---
+++ src/Components2/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>How can we help you?
+    Want to check the status of your order? Want to know how to return a product? We can help. </p>
+        </div>
+    );
+};
+
+export default Contact;
